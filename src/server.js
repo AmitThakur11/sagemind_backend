6e810cc3c7fe87c0213539995823d0b5c0ac711f
amit thakur <amithakur081@gmail.com>
2022-02-19 18:54:54 +0530

    Add database connection

--- src/server.js
+++ src/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const cors = require('cors')
 const {quizes}= require("./quizData")
+const dbConnect = require("./dbConnect")
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
+require("dotenv").config({
+    path : "./data.env"
+})
+
+dbConnect();
 app.get("/",(req,res)=>{
     res.status(200).send("working")
 })
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 3002
 app.get("/quiz",(req,res)=>{
     try{
         res.status(200).json({success : true , quizes : quizes})
