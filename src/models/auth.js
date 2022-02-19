40a59e96c7068b2b688ad30864d85cef7e6c0880
amit thakur <amithakur081@gmail.com>
2022-02-19 18:59:10 +0530

    Create : auth model

---
+++ src/models/auth.js
@@ -0,0 +1,38 @@
+const {Schema , model} = require('mongoose')
+
+const userSchema = new Schema({
+    username : {
+        type : String,
+        required: true
+    },
+    email : {
+        type : String,
+        required : true,
+        unique:true
+    },
+    password : {
+        type : String,
+        required :true,
+        length :{
+            min: 6
+        }
+
+    },
+    quizData : {
+        type : Schema.Types.ObjectId,
+        ref : "QuizData"
+        
+    }
+
+},{
+    timestamps: true
+})
+
+
+const User =  model("User", userSchema);
+
+
+
+
+
+module.exports = {User}
