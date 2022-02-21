242e5a528239f060a8d1cce4ad6939db5a1d8dce
amit thakur <amithakur081@gmail.com>
2022-02-21 14:54:58 +0530

    add cors option

--- src/server.js
+++ src/server.js
@@ -9,7 +9,13 @@ const { getQuiz } = require('./controller/quizData');
 const app = express();
 
 
-app.use(cors());
+const corsOptions ={
+    origin:'*', 
+    credentials:true,        
+    optionSuccessStatus:200,
+ }
+
+app.use(cors(corsOptions));
 app.use(express.json())
 
 require("dotenv").config({
